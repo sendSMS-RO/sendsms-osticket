35f242fedc97fcdb8d758f950364672753bbe54b
Temepest74 <raducata11@yahoo.com>
2021-04-05 17:25:25 +0300

    Update plugin.php

--- plugin.php
+++ plugin.php
@@ -4,8 +4,8 @@ return array(
     'id' => 'sendsms:messaging', # notrans
     'version' => '0.1',
     'name' => 'sendSMS',
-    'author' => 'Radu Vasile Catalin',
+    'author' => 'sendSMS',
     'description' => 'Use our SMS shipping solution to deliver the right information at the right time.',
-    'url' => 'https://www.sendsms.ro/en/', #temporat
+    'url' => 'https://www.sendsms.ro/en/', 
     'plugin' => 'sendsms.php:SendSMS'
 );
